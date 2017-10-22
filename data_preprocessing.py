0f27c88811bc912196b1914fac75ca73b7bdb6eb
Leye <wangleye@gmail.com>
2017-10-22 20:04:34 +0800

    fix bug in generating ht count table

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -25,8 +25,9 @@ def gen_cc_count_table():
         print(e)
         conn.rollback()
 
-def gen_ht_count_table():
-    create_tbl_stat = "create table ht_count as (select idhometown, count(*) as cnt from user where idhometown != '' group by idhometown)"
+def gen_ht_with_cc_count_table():
+    # generate the total number of users for each hometown, where the users are those who also publish current city
+    create_tbl_stat = "create table ht_with_cc_count as (select idhometown, count(*) as cnt from user where idhometown != '' and idcurrentcity != '' group by idhometown)"
     try:
         x = conn.cursor()
         x.execute(create_tbl_stat)
@@ -35,7 +36,9 @@ def gen_ht_count_table():
         print(e)
         conn.rollback()
 
+# def gen_we_
+
 if __name__ == '__main__':
     # gen_indication_cc_ht_count_table()
-    gen_cc_count_table()
+    # gen_cc_count_table()
     gen_ht_count_table()
