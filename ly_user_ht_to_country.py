995af78c9e50201da3ea3930b5346f5e6676eec0
Leye <wangleye@gmail.com>
2017-10-22 14:27:33 +0800

    update database location update

--- ly_user_ht_to_country.py
+++ ly_user_ht_to_country.py
@@ -1,14 +1,18 @@
 # _*_ coding:utf-8 _*_
 import time, pymysql, facebook
 
-# geolocator = Nominatim()
-
-
 conn = pymysql.connect(host= "localhost",
                   user="root",
                   passwd="123456",
-                  db="fb_posts",
-				  charset='utf8')
+                  db="all0504",
+                  charset='utf8')
+
+def read_api_key():
+    lines = []
+    with open('secret') as s:
+        lines = s.readlines()
+    return lines[0].strip(), lines[1].strip()
+
 
 def find_country_from_db(location_id):
     query_statement = "SELECT country FROM fb_location WHERE location_id = '{}'".format(location_id)
@@ -20,23 +24,22 @@ def find_country_from_db(location_id):
     return results[0][0]
 
 def store_FB_loc(loc_id, loc_lat, loc_long, city, country):
-	insert_statement = "INSERT INTO fb_location (location_id, city, country, location_lat, location_long) VALUES (%s,%s,%s,%s,%s)"
-	try:
-		# print(city.encode(), country.encode(), loc_lat, loc_long)
-		# insert_sql = insert_statement.format(loc_id, city, country, loc_lat, loc_long)
-		# print(insert_sql)
-		x = conn.cursor()
-		x.execute(insert_statement, (loc_id, city, country, loc_lat, loc_long))
-		conn.commit()
-		print(x._last_executed.encode())
-	except pymysql.Error as e:
-		print(e)
-		# print("insert ERROR!")
-		conn.rollback()
+    insert_statement = "INSERT INTO fb_location (location_id, city, country, location_lat, location_long) VALUES (%s,%s,%s,%s,%s)"
+    try:
+        # print(city.encode(), country.encode(), loc_lat, loc_long)
+        # insert_sql = insert_statement.format(loc_id, city, country, loc_lat, loc_long)
+        # print(insert_sql)
+        x = conn.cursor()
+        x.execute(insert_statement, (loc_id, city, country, loc_lat, loc_long))
+        conn.commit()
+        print(x._last_executed.encode())
+    except pymysql.Error as e:
+        print(e)
+        # print("insert ERROR!")
+        conn.rollback()
 
 def find_country_from_fb(cc):
-    app_id = '1598399240414220'
-    app_secret = '1bb78f1c54c42e0402a2e4127d6c26b6'
+    app_id, app_secret = read_api_key()
     graph = facebook.GraphAPI(access_token='{}|{}'.format(app_id, app_secret), version='2.3')
     time.sleep(1) # not query too frequently
     city_id = cc
@@ -63,31 +66,27 @@ def find_country_from_fb(cc):
                 break
     print("SHOULD NOT ARRIVE HERE!! cc id: {}".format(cc))
 
+def get_all_hometown_ids():
+    query_statement = "SELECT distinct(idhometown) FROM user where idhometown != ''"
+    x = conn.cursor()
+    x.execute(query_statement)
+    results = x.fetchall()
+    return results
+
+def get_all_cc_ids():
+    query_statement = "SELECT distinct(idcurrentcity) FROM user where idcurrentcity != ''"
+    x = conn.cursor()
+    x.execute(query_statement)
+    results = x.fetchall()
+    return results
+
 if __name__ == '__main__':
 
-	# read user ht map
-	user_ht_map = {}
-	with open('ProfJOC/users_hometown') as inputfile:
-		for line in inputfile:
-			words = line.strip().split('\t')
-			if len(words) == 2:
-				user_ht_map[words[0]] = words[1].strip()
-			elif len(words) == 1:
-				user_ht_map[words[0]] = 'unknown'
-
-	count = 0
-	with open ('ProfJOC/u_ht_country', 'w') as outputfile:
-		for u in user_ht_map.keys():
-			print("user {}: {}".format(count, u))
-			count += 1
-			ht = user_ht_map[u]
-			if ht != 'unknown':
-				country = find_country_from_db(ht)
-				if country is None:
-					country = find_country_from_fb(ht)
-				if country is not None:
-					outputfile.write("{}\t{}\n".format(u, country))
-				else:
-					outputfile.write("{}\t{}\n".format(u, 'None'))
-			else:
-				outputfile.write("{}\t{}\n".format(u, 'None'))
+    all_hometown_ids = get_all_hometown_ids()
+    for idx, result_line in enumerate(all_hometown_ids):
+        ht_id = result_line[0]
+        country = find_country_from_db(ht_id)
+        if country is None:
+            print('{}/{}'.format(idx, len(all_hometown_ids)))
+            print('retreive city {} from Facebook...'.format(ht_id))
+            country = find_country_from_fb(ht_id)
