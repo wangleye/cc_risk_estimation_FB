097c38005179bbd28f3551a987f78d80be5ac3da
Leye <wangleye@gmail.com>
2017-10-22 16:00:41 +0800

    add table creation

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,5 +5,15 @@ conn = pymysql.connect(host= "localhost",
                   passwd="123456",
                   db="all0504")
 
-# generating a list of user ids with current city published
-define 
+def gen_indication_cc_ht_count_table():
+    create_tbl_stat = "create cc_ht_count as select idcurrentcity, idhometown, count(*) from user where cc != '' and ht != '' group by idcurrentcity, idhometown"
+    try:
+        x = conn.cursor()
+        x.execute(create_tbl_stat)
+        conn.commit()
+    except pymysql.Error as e:
+        print(e)
+        conn.rollback()
+
+if __name__ == '__main__':
+    gen_indication_cc_ht_count_table()
