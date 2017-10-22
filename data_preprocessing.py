63f1aebf53d7e3dd1cb646a48643425ceb4eb160
wangleye <wangleye@gmail.com>
2017-10-22 17:20:53 +0800

    add count tables

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,7 +6,27 @@ conn = pymysql.connect(host= "localhost",
                   db="all0504")
 
 def gen_indication_cc_ht_count_table():
-    create_tbl_stat = "create cc_ht_count as select idcurrentcity, idhometown, count(*) from user where cc != '' and ht != '' group by idcurrentcity, idhometown"
+    create_tbl_stat = "create table cc_ht_count as (select idcurrentcity, idhometown, count(*) as cnt from user where idcurrentcity != '' and idhometown != '' group by idcurrentcity, idhometown)"
+    try:
+        x = conn.cursor()
+        x.execute(create_tbl_stat)
+        conn.commit()
+    except pymysql.Error as e:
+        print(e)
+        conn.rollback()
+
+def gen_cc_count_table():
+    create_tbl_stat = "create table cc_count as (select idcurrentcity, count(*) as cnt from user where idcurrentcity != '' group by idcurrentcity)"
+    try:
+        x = conn.cursor()
+        x.execute(create_tbl_stat)
+        conn.commit()
+    except pymysql.Error as e:
+        print(e)
+        conn.rollback()
+
+def gen_ht_count_table():
+    create_tbl_stat = "create table ht_count as (select idhometown, count(*) as cnt from user where idhometown != '' group by idhometown)"
     try:
         x = conn.cursor()
         x.execute(create_tbl_stat)
@@ -16,4 +36,6 @@ def gen_indication_cc_ht_count_table():
         conn.rollback()
 
 if __name__ == '__main__':
-    gen_indication_cc_ht_count_table()
+    # gen_indication_cc_ht_count_table()
+    gen_cc_count_table()
+    gen_ht_count_table()
